73dccc18ca552a1cc0356ed8f52396c765a993f0
inspire-man <inspire-man@users.noreply.github.com>
2026-09-09 03:18:53 +0000

    deploy: f518508909cd9d172a91da3a865c889443442865

---
+++ assets/chunks/swimlanesDiagram-VR7AAH4N.87anCGQP.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-HODETNUW.H-00Gz5J.js";import{_ as a}from"../app.B13GQGzS.js";import"./chunk-5VM5RSS4.CRk-uLHk.js";import"./chunk-XXDRQBXY.BH2g1oFm.js";import"./chunk-POPQ4Y6H.De5AuZkL.js";import"./chunk-F27PBJKO.DO_DCjnS.js";import"./channel.Dh48gFyQ.js";import"./framework.BC3NdntU.js";import"./theme.CbFQI-FK.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
