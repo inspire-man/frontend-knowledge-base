4791481bc9a1426b1b374f980ed4cdc50d4de4d9
inspire-man <inspire-man@users.noreply.github.com>
2026-09-10 10:36:28 +0000

    deploy: cf1d6bfee6d927efb41ceb054ec683730c7730a4

---
+++ assets/chunks/channel.3vC8sLoz.js
@@ -0,0 +1,1 @@
+import{U as a,C as n}from"../app.Ki3StIXo.js";const t=(r,o)=>a.lang.round(n.parse(r)[o]);export{t as c};
---
+++ assets/chunks/swimlanesDiagram-VR7AAH4N.BBfmyGe5.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-HODETNUW.DzirTsVT.js";import{_ as a}from"../app.Ki3StIXo.js";import"./chunk-5VM5RSS4.f5mU5wg7.js";import"./chunk-XXDRQBXY.CUAOlRFZ.js";import"./chunk-POPQ4Y6H.CrFLcc25.js";import"./chunk-F27PBJKO.CfiuaPEt.js";import"./channel.3vC8sLoz.js";import"./framework.BC3NdntU.js";import"./theme.egGZjNbJ.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
